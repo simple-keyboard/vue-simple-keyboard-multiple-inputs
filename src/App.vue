0d324954b1bb111362438a8b008cbd8461dd59bf
simple-keyboard <46766174+simple-keyboard@users.noreply.github.com>
2019-09-16 22:59:42 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div v-for="key in Object.keys(inputs)" :key="key">
+      <Input
+        :inputs="inputs"
+        :inputName="key"
+        @onInputFocus="onInputFocus"
+        @onInputChange="onInputChange"
+      />
+    </div>
+
+    <SimpleKeyboard
+      @onChange="onChange"
+      @onKeyPress="onKeyPress"
+      :input="inputs[inputName]"
+      :inputName="inputName"
+    />
+  </div>
+</template>
+
+<script>
+import SimpleKeyboard from "./SimpleKeyboard";
+import Input from "./Input";
+import "./App.css";
+
+export default {
+  name: "App",
+  components: {
+    SimpleKeyboard,
+    Input
+  },
+  data: () => ({
+    /**
+     * We define the inputs here
+     */
+    inputs: {
+      input1: "",
+      input2: ""
+    },
+    inputName: "input1"
+  }),
+  methods: {
+    onChange(input) {
+      this.inputs[this.inputName] = input;
+    },
+    onKeyPress(button) {
+      console.log("button", button);
+    },
+    onInputChange(input) {
+      console.log("Input changed directly:", input.target.id);
+      this.inputs[input.target.id] = input.target.value;
+    },
+    onInputFocus(input) {
+      console.log("Focused input:", input.target.id);
+      this.inputName = input.target.id;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/Input.vue
@@ -0,0 +1,40 @@
+<template>
+  <input
+    :id="inputName"
+    :value="inputValue"
+    :placeholder="'Input: '+ inputName"
+    @focus="onInputFocus"
+    @input="onInputChange"
+  >
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    inputs: {
+      type: Object
+    },
+    inputName: {
+      type: String
+    }
+  },
+  methods: {
+    onInputChange(input) {
+      this.$emit("onInputChange", input);
+    },
+    onInputFocus(input) {
+      this.$emit("onInputFocus", input);
+    }
+  },
+  computed: {
+    inputValue: function () {
+      return this.inputs[this.inputName];
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
